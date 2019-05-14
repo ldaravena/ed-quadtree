b75582c9b59509fc29252ea5c3fac0faa726c6f9
Leonardo Aravena Cuevas <ld.aravena@gmail.com>
2019-05-14 12:04:54 -0400

    Add files via upload

---
+++ PR.h
@@ -0,0 +1,31 @@
+#include "PR_ADT.h"
+
+struct nodo{
+
+	int x;
+	int y;
+
+	nodo *padre;
+	nodo *nw;
+	nodo *sw;
+	nodo *se;
+	nodo *ne;
+};
+
+struct punto{
+
+	int x;
+	int y;
+};
+
+class PR: public PR_Tree{
+
+	private:
+		
+		nodo raiz;
+		
+	public:
+		
+		PR(int n,int m,std::vector<punto> puntos);
+		bool search(int x, int y);
+};
