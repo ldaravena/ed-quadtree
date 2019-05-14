64ebb38aaa649ffe6c175c41acd59cdc4eb18382
Ignacio Brito Gomez <50630904+nachobri99@users.noreply.github.com>
2019-05-14 11:55:29 -0400

    Add files via upload

--- PR_ADT.h
+++ PR_ADT.h
@@ -1,6 +1,14 @@
 #ifndef PR_ADT_
 #define PR_ADT_
 
+#include <vector>
+class PR_Tree{
+
+	public:
+
+		virtual PR_Tree(int, int, std::vector<int>)=0;
+		virtual search(int, int)= 0;
+};
 
 
 #endif
